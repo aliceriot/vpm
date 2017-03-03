8b99c125a20d36efbdcfe10af790c372616f9c15
Alice Pote <alice.writes.wrongs@gmail.com>
2017-03-03 15:54:51 -0500

    I have no idea what I'm doing

--- src/Data/JSON.hs
+++ src/Data/JSON.hs
@@ -1,8 +1,16 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+
 module JSON
   (
   ) where
+
+import Data.Aeson
 
 data VPMConfig = VPMConfig
     { use_lockfile :: Bool
     , vpm_version :: String
     } deriving (Show)
+
+instance FromJSON VPMConfig
+
+instance ToJSON VPMConfig
