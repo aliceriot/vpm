d0ae328fc16b6bf96e56c4709613ad6701d9df59
Alice Pote <alice.writes.wrongs@gmail.com>
2017-03-03 16:49:52 -0500

    ok I don't understand haskell yet

--- src/Data/JSON.hs
+++ src/Data/JSON.hs
@@ -5,12 +5,21 @@ module JSON
   ) where
 
 import Data.Aeson
+import GHC.Generics
+
+-- import Data.Text
+import Control.Applicative
+import Control.Monad
+import qualified Data.ByteString.Lazy as B
 
 data VPMConfig = VPMConfig
     { use_lockfile :: Bool
     , vpm_version :: String
-    } deriving (Show)
+    } deriving (Show, Generic)
 
 instance FromJSON VPMConfig
 
 instance ToJSON VPMConfig
+
+getJSON :: FilePath -> IO B.ByteString
+getJSON = B.readFile
